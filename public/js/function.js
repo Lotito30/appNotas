9dea8d80f334488bb52d1ec34ecb7429f82b1c9c
Lotito30 <dominguezje21@gmail.com>
2023-05-08 23:35:29 -0300

    update funciones en js y eliminacion de un elemento de lista del navbar en html, transform en css

--- public/js/function.js
+++ public/js/function.js
@@ -1,20 +1,22 @@
 
-function cambiarDisplayNuevo(){
-    
-    var contenedor = document.getElementById("contenedor")
-    var formulario = document.getElementById("formulario")
-        formulario.style.display = "block";
-        contenedor.style.display = "none";
+// Cambiar el valor del estilo "display" de los elementos "contenedor" y "formulario" para mostrar el formulario
+function cambiarDisplayNuevo() {
+    // Obtener los elementos "contenedor" y "formulario"
+var contenedor = document.getElementById("contenedor");
+var formulario = document.getElementById("formulario");
 
+  contenedor.style.display = "none";
+  formulario.style.display = "block";
 }
 
+// Cambiar el valor del estilo "display" de los elementos "contenedor" y "formulario" para ocultar el formulario
 function cambiarDisplayCancelarYGuardar() {
-    var contenedor = document.getElementById("contenedor")
-    var formulario = document.getElementById("formulario")
+    // Obtener los elementos "contenedor" y "formulario"
+var contenedor = document.getElementById("contenedor");
+var formulario = document.getElementById("formulario");
 
-    formulario.style.display = "none";
-    contenedor.style.display = "block";
-
+  contenedor.style.display = "block";
+  formulario.style.display = "none";
 }
 function guardar(){
     // obtener el valor de los campos de titulo y textarea
@@ -58,7 +60,6 @@ function crearDivConDosP(titulo,textarea){
     const contenedor = document.getElementById("guardada");
     contenedor.appendChild(nuevoDiv);
 }
-
 function borrarCampos(){
     document.getElementById("titulo").value = ''
     document.getElementById("textarea").value = ''
