e995de6b90073ef1814eac47066da74806929ca2
Lotito30 <dominguezje21@gmail.com>
2023-06-07 11:01:11 -0300

    actualizar footer, volver clase 07_06

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,9 +1,9 @@
 const path = require('path')
 
 const mainController = {
-    index:(req, res) => {
+      index:(req, res) => {
         res.sendFile(path.join(__dirname, '../views/index.html'))
-      },
+    },
     preguntas:(req, res) => {
         res.sendFile(path.join(__dirname, '../views/preguntas.html'))
       },
