d3654eab69c8e4ff07055f43568009511fcc3da2
Jesus Dominguez <112903235+Lotito30@users.noreply.github.com>
2023-06-13 20:15:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const express = require('express')
-const PORT = 80 || 443
+const PORT = process.env.PORT || 3000
 const app = express()
 app.use(express.static('public'));
 
